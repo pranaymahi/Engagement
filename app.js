9e25cbeda16b24db2127845f8b8aa1c52e1ce90f
pranaymahi <39729470+pranaymahi@users.noreply.github.com>
2025-09-17 13:03:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Countdown Timer
 function updateCountdown() {
-    const weddingDate = new Date("December 25, 2025 00:00:00").getTime();
+    const weddingDate = new Date("November 15, 2025 00:00:00").getTime();
     const now = new Date().getTime();
     const timeRemaining = weddingDate - now;
 
